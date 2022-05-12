a24377944467b133e1ca77b1495555d1fb44c5b1
Jasmine Bergamo <jasmineticc@gmail.com>
2022-05-12 17:47:13 -0300

    Adição js, css e conclusão do projeto

---
+++ AluraConf/js/index.js
@@ -0,0 +1,15 @@
+ativaScrollSuave('a[href*=sobre]');
+ativaScrollSuave('a[href*=palestrantes]');
+ativaScrollSuave('a[href*=inscricao');
+function ativaScrollSuave(selector) {
+
+    $(selector).click(function(event){
+        event.preventDefault();
+
+        var target = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 1000);  
+    });
+}
