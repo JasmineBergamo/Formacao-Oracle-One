dee777c6c9544ab2aa6f260b5650137f44201902
Jasmine Bergamo <jasmineticc@gmail.com>
2022-05-23 15:56:37 -0300

    + arquivos Java

---
+++ Java/sintaxe Variaveis e Fluxo/src/TestaAConversao.java
@@ -0,0 +1,31 @@
+
+public class TestaAConversao {
+	public static void main (String[] args) {
+		double salario = 1270.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		
+		float pontoFlutuante = 3.14f;
+		
+		System.out.println(pontoFlutuante);
+		
+		int x = 2;
+		
+		System.out.println(x);
+		
+		long numeroGrande = 321135231;
+		
+		System.out.println(numeroGrande);
+		
+		short valorPequeno = 2131;
+		
+		System.out.println(valorPequeno);
+		
+		byte b = 127;
+		System.out.println(b);
+		
+		
+	}
+	
+}
